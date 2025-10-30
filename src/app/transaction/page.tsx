2a68ba5f9b0497f1a55588033aeb938cebec68b7
Helen Cris <helencris_morais@hotmail.com>
2025-10-30 17:12:10 -0300

    ✨ feat: created transaction page

---
+++ src/app/transaction/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import { Text } from '@grupo10-pos-fiap/design-system';
+import React from 'react';
+
+const Transactions: React.FC = () => {
+  return <Text>Transações</Text>;
+}
+
+export default Transactions;
