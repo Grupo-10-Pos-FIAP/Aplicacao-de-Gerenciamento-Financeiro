46a1dc9de2d1ce752d00ece1bfd8df7c19384cfe
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2025-11-04 01:56:48 -0300

    feat: tratativa de erros e registro de rota

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,35 +1,75 @@
-export class TransactionDeleteError extends Error {
+export class ApiError extends Error {
   constructor(
     message: string,
     public code?: string,
     public statusCode?: number
   ) {
     super(message);
+    this.name = 'ApiError';
+    Object.setPrototypeOf(this, ApiError.prototype);
+  }
+}
+
+export class TransactionError extends ApiError {
+  constructor(
+    message: string,
+    public code?: string,
+    public statusCode?: number
+  ) {
+    super(message, code, statusCode);
+    this.name = 'TransactionError';
+    Object.setPrototypeOf(this, TransactionError.prototype);
+  }
+}
+
+export class TransactionCreateError extends TransactionError {
+  constructor(
+    message: string = 'Erro ao criar transação.',
+    code: string = 'CREATE_ERROR',
+    statusCode?: number
+  ) {
+    super(message, code, statusCode);
+    this.name = 'TransactionCreateError';
+    Object.setPrototypeOf(this, TransactionCreateError.prototype);
+  }
+}
+
+export class TransactionDeleteError extends TransactionError {
+  constructor(
+    message: string = 'Erro ao deletar transação.',
+    code: string = 'DELETE_ERROR',
+    statusCode?: number
+  ) {
+    super(message, code, statusCode);
     this.name = 'TransactionDeleteError';
     Object.setPrototypeOf(this, TransactionDeleteError.prototype);
   }
 }
 
-export class UnauthorizedDeleteError extends TransactionDeleteError {
-  constructor(message: string = 'Esta transação não pode ser deletada pois já foi finalizada.') {
-    super(message, 'UNAUTHORIZED_DELETE', 403);
-    this.name = 'UnauthorizedDeleteError';
-    Object.setPrototypeOf(this, UnauthorizedDeleteError.prototype);
+export class NotFoundError extends ApiError {
+  constructor(message: string = 'Transação não encontrada.') {
+    super(message, 'NOT_FOUND', 404);
+    this.name = 'NotFoundError';
+    Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 }
 
-export class NetworkError extends TransactionDeleteError {
-  constructor(message: string = 'Erro de conexão. Verifique sua internet e tente novamente.') {
+export class NetworkError extends ApiError {
+  constructor(
+    message: string = 'Erro de conexão. Verifique sua internet e tente novamente.'
+  ) {
     super(message, 'NETWORK_ERROR', 0);
     this.name = 'NetworkError';
     Object.setPrototypeOf(this, NetworkError.prototype);
   }
 }
 
-export class NotFoundError extends TransactionDeleteError {
-  constructor(message: string = 'Transação não encontrada.') {
-    super(message, 'NOT_FOUND', 404);
-    this.name = 'NotFoundError';
-    Object.setPrototypeOf(this, NotFoundError.prototype);
+export class UnauthorizedDeleteError extends TransactionDeleteError {
+  constructor(
+    message: string = 'Esta transação não pode ser deletada pois já foi finalizada.'
+  ) {
+    super(message, 'UNAUTHORIZED_DELETE', 403);
+    this.name = 'UnauthorizedDeleteError';
+    Object.setPrototypeOf(this, UnauthorizedDeleteError.prototype);
   }
 }
